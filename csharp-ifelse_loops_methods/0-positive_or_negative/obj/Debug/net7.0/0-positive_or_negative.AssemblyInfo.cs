d643c10b27fb39fbfe4a45163a053b80c48614c6
EucTech <euccreative@gmail.com>
2026-09-12 12:25:41 -0400

    fix: update the code logic

--- csharp-ifelse_loops_methods/0-positive_or_negative/obj/Debug/net7.0/0-positive_or_negative.AssemblyInfo.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/obj/Debug/net7.0/0-positive_or_negative.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0-positive_or_negative")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db1366851b53a8eaee2000a4c78a697a48d1dab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad7e3d01e442cfda1355a8a4f0c43922f120c24")]
 [assembly: System.Reflection.AssemblyProductAttribute("0-positive_or_negative")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0-positive_or_negative")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
